c8248b48be77decfbb6dcfb29e5fcf0fd79806f1
philip-ifeanyi <philipifeanyi2000@gmail.com>
2023-11-15 13:28:14 +0100

    worked on uploads

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const multer = require('multer');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads' })
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,13 +19,34 @@ mongoose
   .then(()=> console.log("Database connection established!"))
   .catch((err)=> console.error(err));
 
+const fileSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  size: Number
+});
+const File = mongoose.model('File', fileSchema);
+
 // Homepage
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  const { originalname, mimetype, size } = req.file
+  const fileObj = new File({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
 
+  const file = await fileObj.save()
 
+  res.json({
+    name: file.name,
+    type: file.type,
+    size: file.size
+  })
+})
 
 // Set App to listen on port
 app.listen(port, function () {
